54889075d673147b58e7fd15d0da99ecdd583662
Achsah Abraham <achsahmp@gmail.com>
2018-10-17 15:36:47 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -24,9 +24,14 @@ class MusicLibraryController
   end
 
   def list_songs
-      Song.all.sort_by{|song| song.name}.uniq.each.with_index(1) do |song, name|
-        puts "#{name}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-      end
+    Song.all.sort_by{|song| song.name}.uniq.each.with_index(1) do |song, name|
+      puts "#{name}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
+  end
 
+  def list_artists
+    Artist.all.sort_by{|artist| artist.name}.uniq.each.with_index(1) do |artist, name|
+      puts "#{index}. #{artist.name}"
+    end
+  end
 end
